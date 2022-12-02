fe9251ef58c1a2ed9bdbce8346764c432a4b2bd1
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-12-02 09:00:45 +0100

    Update Cabezon minecraft.py

--- Cabezon minecraft.py
+++ Cabezon minecraft.py
@@ -5,79 +5,40 @@ from discord.ext import commands
 from PIL import Image, ImageDraw, ImageFont
 import io
 import time
-import os
-import urllib.request
+
 
 bot = commands.Bot(command_prefix='!', description="ayuda bot") #Comando
 bot.remove_command("help") # Borra el comando por defecto !help
 
 @bot.command()
-async def Skin(ctx, *, SkinMinecraft):
+async def skin(ctx, *, SkinMinecraft):
+    await ctx.message.delete()
+    await ctx.send(f"Generando Skin {SkinMinecraft}...", delete_after=0)
+    time.sleep(3)
    
-    response = requests.get(f"https://jose89fcb.es/minecraft/3D/skin/CabezonMinecraft/p/skins/skin.php?cabezon={SkinMinecraft}")
-    imagen = response.json()['CabezonMinecraft']
-
-    url_imagen = f"{imagen}"
-    
-    nombre_local_imagen = f"Skin-{SkinMinecraft}.png"
-    
-    
-    imagen = requests.get(url_imagen).content
-
-
-    
    
-    with open(nombre_local_imagen, 'wb') as handler:
-        handler.write(imagen)
-
-
-    skin = imagen
-    # cargo la imagen desde la memoria
-    img1 = Image.open(io.BytesIO(requests.get(url_imagen).content)).convert("RGBA")
-
-   
-
-
-
+    url = f"https://jose89fcb.es/minecraft/3D/skin/CabezonMinecraft/p/skins/CabezonMinecraft.php?usuario={SkinMinecraft}"
+    imagen = Image.open(io.BytesIO(requests.get(url).content))
+    r = requests.get(url)
+    if  r.status_code ==200:
+        imagen = Image.open(io.BytesIO(requests.get(url).content))
+        with io.BytesIO() as imagen_binary:
+            imagen.save(imagen_binary, 'PNG')
+            imagen_binary.seek(0)
+            embed=discord.Embed(title="Skin Minecraft", url="https://jose89fcb.es", description=f"Skin - {SkinMinecraft}")
     
-
-    
-    
-   
-    
-
-
-    
-    
- 
-
-    
-    
-
-    with io.BytesIO() as image_binary:
-       
-
-        img1.save(image_binary, 'PNG')
-        
-       
-         
-        image_binary.seek(0)
-        
-       
-  
-        embed = discord.Embed(title=f"{SkinMinecraft}", description=f"{SkinMinecraft}", color=discord.Colour.random()) 
-        
-       
-   
-
-        embed.set_image(url=f"attachment://Skin-{SkinMinecraft}.png")
-        await ctx.send(embed=embed, file=discord.File(fp=image_binary, filename=f"Skin-{SkinMinecraft}.png"))
-        os.remove(f"Skin-{SkinMinecraft}.png")
-       
-   
+            embed.set_image(url=f"attachment://Skin-{SkinMinecraft}.png")
+            embed.set_footer(text="BOT Programado Por Jose89fcb")
+            await ctx.send(embed=embed, file=discord.File(fp=imagen_binary, filename=f"Skin-{SkinMinecraft}.png"))
+    else:
+        await ctx.send(f":(")
 
 
-        
+@skin.error
+async def skin_error(ctx, error):
+        if isinstance(error, commands.errors.CommandInvokeError):
+            await ctx.send("La skin no existe!")        
+
 
    
 
@@ -87,4 +48,4 @@ async def Skin(ctx, *, SkinMinecraft):
 async def on_ready():
     print("BOT listo!")
     
-bot.run('') #OBTEN UN TOKEN EN: https://discord.com/developers/application
+bot.run('') 
